0f2db493c1a7d4e995da6c2aa339983fe5b25bc8
Chloe Raison <cloverena24@gmail.com>
2023-06-18 18:40:51 +0100

    amend response

---
+++ calculatorSolution/calculatorApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+public class ArithmeticOperations
+{
+
+    public static void Main(String[] args)
+    {
+        PerformOperation();
+
+    }
+
+    public static void PerformOperation()
+    {
+        Console.WriteLine("beep boop first number required");
+        string? input1 = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input1))
+        {
+            Console.WriteLine("Invalid input");
+            return;
+        }
+
+        double num1;
+        if (!double.TryParse(input1, out num1))
+        {
+            Console.WriteLine("Invalid number format");
+            return;
+        }
+
+        Console.WriteLine("second number required beep boop");
+        string? input2 = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input2))
+        {
+            Console.WriteLine("Invalid input");
+            return;
+        }
+
+        double num2;
+        if (!double.TryParse(input2, out num2))
+        {
+            Console.WriteLine("Invalid number format");
+            return;
+        }
+
+        Console.WriteLine("beep requires operator: +, -, *, or /, boop");
+        string? operationInput = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(operationInput) || operationInput.Length != 1)
+        {
+            Console.WriteLine("Invalid operation input");
+            return;
+        }
+
+        char operation = operationInput[0];
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                result = num1 / num2;
+                break;
+
+            default:
+                Console.WriteLine("Invalid operation");
+                return;
+        }
+
+        Console.WriteLine("beep boop result: " + result);
+    }
+}
